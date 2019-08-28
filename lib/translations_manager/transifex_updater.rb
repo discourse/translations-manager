68bbf2a99412638b73057935c89c6b2e1945e092
Gerhard Schlager <mail@gerhard-schlager.at>
2019-08-28 21:09:01 +0200

    Allow plugins to pull languages which are not part of Discourse

--- lib/translations_manager/transifex_updater.rb
+++ lib/translations_manager/transifex_updater.rb
@@ -29,14 +29,16 @@ module TranslationsManager
       @languages = (languages.empty? ? SUPPORTED_LOCALES : languages).select { |x| x != 'en' }.sort
     end
 
-    def perform(pull: true, tx_config_filename: '.tx/config')
-      update_tx_config(tx_config_filename)
+    def perform(pull: true, update_tx_config: true, tx_config_filename: '.tx/config', language_map: {})
+      @additional_language_map = language_map
+
+      update_tx_config(tx_config_filename) if update_tx_config
       create_missing_locale_files
       pull_translations if pull
 
       @yml_dirs.each do |dir|
         @yml_file_prefixes.each do |prefix|
-          @languages.each do |language|
+          mapped_languages.each do |language|
             filename = yml_path_if_exists(dir, prefix, language)
 
             if filename
@@ -88,5 +90,12 @@ module TranslationsManager
       end
     end
 
+    def mapped_languages
+      language_map = LANGUAGE_MAP.merge(@additional_language_map)
+
+      @languages.map do |language|
+        language_map.fetch(language, language)
+      end
+    end
   end
 end
