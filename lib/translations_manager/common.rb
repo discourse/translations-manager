discourse/translations-manager

commit 6b17cf297efa9896c9fdd1367ec16d451ec82067

Stop using open3. We want to see the output of the tx client

lib/translations_manager/common.rb
@@ -1,4 +1,3 @@
-require 'open3'
 require_relative 'transifex_config_file_updater'
 
 module TranslationsManager
@@ -32,16 +31,7 @@ module TranslationsManager
     end
 
     def execute_tx_command(command)
-      return_value = Open3.popen2e(command) do |_, stdout_err, wait_thr|
-        while (line = stdout_err.gets)
-          puts line
-        end
-        wait_thr.value
-      end
-
-      puts ''
-
-      unless return_value.success?
+      unless system(command)
         STDERR.puts 'Something failed. Check the output above.', ''
         exit return_value.exitstatus
       end

commit d6da5ff928b9ea39a15c9b6f16b856f46bc9069b

FIX: Update a leftover variable reference
Broken in 6b17cf297efa9896c9fdd1367ec16d451ec82067

lib/translations_manager/common.rb
@@ -35,7 +35,7 @@ module TranslationsManager
     def execute_tx_command(command)
       unless system(command)
         STDERR.puts 'Something failed. Check the output above.', ''
-        exit return_value.exitstatus
+        exit $?.exitstatus
       end
     end
 